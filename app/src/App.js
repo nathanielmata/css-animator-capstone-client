acec6e51ce1029797d1b684a4f922b50d01f46ee
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-10-31 14:01:17 -0700

    basic form for generating animation css

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
+function App() {
+	// Declare a new state variable, which we'll call "animation"
+	let [animation, setAnimation] = useState('');
 
-  handlePlay = (e) => {
-    document.querySelector('.App-logo').classList.remove('slide-top');
-    void document.querySelector('.App-logo').offsetWidth;
+	const handleClick = (e) => {
+    e.preventDefault();
+    
+		const {
+			name,
+			delay,
+			direction,
+			timing,
+			duration,
+			fill,
+			iteration,
+		} = e.target;
 
-    document.querySelector('.App-logo').classList.add('slide-top');
-  }
+		const str = [
+      duration.value + "ms",
+      timing.value,
+			delay.value + "ms",
+      iteration.value,
+      direction.value,
+			fill.value,
+			name.value,
+		].join(' ');
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={(e) => this.handlePlay(e)}>play</button>
-  
-          <img src={logo} className="App-logo slide-top" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          
-        </header>
-      </div>
-    );
-  }
+		setAnimation(str);
+	};
+
+	return (
+		<div className='App'>
+			<form id='controls' className='controls' onSubmit={(e) => handleClick(e)}>
+				<label for='name'>
+					<div>Name</div>
+					<input type='text' id='name' name='name' />
+				</label>
+
+				<label for='delay'>
+					<div>Delay</div>
+					<input type='range' id='delay' name='delay' min='0' max='6000' value='0' />
+				</label>
+
+				<label for='direction'>
+					<div>Direction</div>
+					<select name='direction' id='direction'>
+						<option value='normal'>normal</option>
+						<option value='reverse'>reverse</option>
+						<option value='alternate'>alternate</option>
+						<option value='alternate-reverse'>alternate-reverse</option>
+					</select>
+				</label>
+
+				<label for='timing'>
+					<div>Timing Function</div>
+					<select name='timing' id='timing'>
+						<option value='ease'>ease</option>
+						<option value='linear'>linear</option>
+						<option value='ease-in'>ease-in</option>
+						<option value='ease-out'>ease-out</option>
+            <option value='ease-in-out'>ease-in-out</option>
+					</select>
+				</label>
+
+				<label for='duration'>
+					<div>Duration</div>
+					<input type='range' id='duration' name='duration' min='0' max='6000' value='3000' />
+				</label>
+
+				<label for='fill'>
+					<div>Fill</div>
+					<select name='fill' id='fill'>
+						<option value='forwards'>forwards</option>
+						<option value='backwards'>backwards</option>
+						<option value='both' selected='selected'>both</option>
+						<option value='none'>none</option>
+					</select>
+				</label>
+
+				<label for='iteration'>
+					<div>Iteration</div>
+					<input
+						type='number'
+						id='iteration'
+						name='iteration'
+						min='1'
+						value='1'
+					/>
+				</label>
+				<button type='submit'>BUTTON</button>
+
+				<div>{animation}</div>
+			</form>
+		</div>
+	);
 }
 
 export default App;
---
+++ app/src/config.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+  TOKEN_KEY: 'markdown-notes-client-auth-token',
+}
