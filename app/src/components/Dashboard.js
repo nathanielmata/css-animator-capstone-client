f68f0004cb4f7a7b678f68da0724e10f1ef5ab79
SultanaK <kr.shampa@yahoo.com>
2020-11-03 22:06:19 -0600

    ading login and registration

---
+++ app/src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class Dashboard extends React.Component {
+
+
+    render() {
+        
+        return (
+            <div className="main__dashboard">
+                <h1>Latest Animations</h1>
+               
+            </div>
+        );
+    }
+};
+
+export default Dashboard
