83b4386e2b0c40bb8154c7120b464fa01d5fdc1e
SultanaK <kr.shampa@yahoo.com>
2020-11-19 23:52:03 -0600

    add not found

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 const config = {
-	API_ENDPOINT:
-	process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
+	API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
 	TOKEN_KEY: 'animation-station-auth-token',
 };
 export default config;
